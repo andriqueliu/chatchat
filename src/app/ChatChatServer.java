75e0498eda6f2b814aef5977012c690919a68dc8
andriqueliu <andriqueliu@gmail.com>
2018-01-07 22:23:06 -0500

    Add members to server

--- src/app/ChatChatServer.java
+++ src/app/ChatChatServer.java
@@ -1,6 +1,14 @@
 package app;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChatChatServer {
+	
+	private Set<String> usernames = new HashSet<String>();
+	private ArrayList<PrintWriter> serverOut = new ArrayList<PrintWriter>();
 	
 	public ChatChatServer() {
 		
